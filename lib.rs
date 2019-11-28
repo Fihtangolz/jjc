65c3b3d05d85946d406078978d1ebfefe995b88c
Dmitry <mamhigtt@gmail.com>
2019-11-29 06:50:16 +0700

    add blunck fn for inplace constructor, add blunck debug, add clone, add iter

--- lib.rs
+++ lib.rs
@@ -1,3 +1,4 @@
 #![feature(allocator_api)]
 #![feature(ptr_internals)]
+#![feature(maybe_uninit_extra)]
 pub mod rbuff;
--- rbuff/mod.rs
+++ rbuff/mod.rs
@@ -1,9 +1,11 @@
 use std::alloc::{Alloc, Global};
-use std::mem;
+use std::ops::Index;
+use core::slice::SliceIndex;
 use std::ptr;
 use std::fmt;
+use std::mem::MaybeUninit;
 
-pub struct RingBuffer<T, A: Alloc = Global> {
+pub struct RingBuffer<T> {
     /// The internal buffer used for storing elements in the container
     buf: ptr::Unique<T>,
     /// The offset to buffer's end
@@ -13,13 +15,13 @@ pub struct RingBuffer<T, A: Alloc = Global> {
     /// The offset to virtual end of the container
     last: usize,
 
-    a: A,
+    a: Global,
 }
 
 impl<T> RingBuffer<T> {
     // Construction
 
-    /// Create an empty container with zero capacity
+    /// Create an empty invariant container with zero capacity
     pub fn new() -> RingBuffer<T> {
         return RingBuffer {
             buf: ptr::Unique::empty(),
@@ -47,6 +49,10 @@ impl<T> RingBuffer<T> {
         }
 
         return t;
+    }
+
+    pub fn new_inplace(raw: *mut u32) -> RingBuffer<T> {
+        unimplemented!();
     }
 
     fn increment(limit: usize, offset: &mut usize) {
@@ -116,7 +122,7 @@ impl<T> RingBuffer<T> {
 
     /// Is the internal buffer is linearized into a continuous array
     pub fn is_linearized(&self) -> bool {
-        // Element are not separated by two part (at start or end in internal buffer) 
+        // Elements are not separated by two part (at start or end in internal buffer) 
         return self.first < self.last 
         // All element placed at the end of internal buffer 
         || self.last == 0;
@@ -129,25 +135,159 @@ impl<T> RingBuffer<T> {
         if self.is_linearized() {
             return;
         }
+
     }
 }
 
-// impl<T> Drop for RingBuffer<T> {
-//     fn drop(&mut self) {
-//         println!("Dropping!");
+impl<T: fmt::Debug> fmt::Debug for RingBuffer<T> {
+    //TODO: expected implementation
+    //
+    //      [1, 2, 3, 4, 5, 6]
+    //       ^           ^ 
+    //       f           l 
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use std::string::String;
+
+        struct SublinePlacement {
+            len: usize,
+            offset: usize,
+        }
+
+        let mut sb_first = SublinePlacement{len:0, offset:0};
+        let mut sb_last = SublinePlacement{len:0, offset:0};
+        let mut ret = std::string::String::new();
+
+        write!(f, "[");
+        for indx in 1..=self.cap {
+            
+            unsafe {
+                let el = self.buf.as_ptr().offset(indx as isize);
+                // let st = "XCD";
+                fmt::Debug::fmt(&*el, f); 
+            }
+            if indx != self.cap { write!(f, ", "); }
+
+            if self.first == indx-1 {
+                // sb_first.len = st.len();
+                sb_last.offset = ret.len();
+            }
+
+            if self.last == indx {
+                // sb_last.len = st.len();
+                sb_last.offset = ret.len();
+            }
+
+            // ret.push_str(st);
+        }
+        // let mut koko = String::from(" ").repeat(tet.offset+1);
+        // let mut ui = koko.clone();
+        // ui.push_str(&String::from("^").repeat(tet.len));
+        // let mut bu = koko.clone();
+        // bu.push('l');
+        return write!(f, "]");
+    }
+}
+
+// impl<T,I> Index<I> for RingBuffer<T> {
+//     type Output;
+
+//     fn index(&self, index: I) -> &Self::Output {
+//        unimplemented!();
 //     }
 // }
 
-// impl<T> fmt::Debug for RingBuffer<T> {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         write!(f, "RingBuffer = []", )
-//     }
-// }
+impl<T> Clone for RingBuffer<T> {
+    fn clone(&self) -> Self {
+        unsafe {
+            let mut copy: RingBuffer<T> = MaybeUninit::uninit().read();
+       
+            copy.cap = self.cap;
+            copy.first = self.first;
+            copy.last = self.last;  
+            copy.a = self.a;
+            
+            let res: ptr::NonNull<T> = copy.a.alloc_array(copy.cap).ok().unwrap();
+            copy.buf = ptr::Unique::new_unchecked(res.as_ptr());
+
+            std::ptr::copy_nonoverlapping(self.buf.as_ptr(), copy.buf.as_ptr(), self.capacity()); 
+            //TODO: may be optimized
+
+            return copy;
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        if self.capacity() >= source.capacity() {
+            //TODO: just copy internals, also all object need to be correct droped
+        }
+        //TODO: ...
+    }
+} 
+
+pub struct IterStateHolder<'a, T> {
+    ptr: *const T,
+    cur: usize,
+    last: usize,
+    phantom: &'a std::marker::PhantomData<T>
+}
+
+impl<'a, T> Iterator for IterStateHolder<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur < self.last { //TODO: incorect implementation
+            unsafe { 
+                let el = self.ptr.offset(self.cur as isize);
+                self.cur += 1;
+                return Some(&*el);
+            } 
+        }
+
+        return None;
+    }
+}
+
+impl<'a, T> IntoIterator for  &'a RingBuffer<T> {
+    type Item = &'a T;
+    type IntoIter = IterStateHolder<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        return IterStateHolder{
+            ptr: self.buf.as_ptr(),
+            cur: self.first,
+            last: self.last,
+            phantom: &std::marker::PhantomData
+        }
+    }
+}
+
+impl<T> Drop for RingBuffer<T> {
+    fn drop(&mut self) {
+        unsafe {
+            let ptr = ptr::NonNull::new_unchecked(self.buf.as_ptr());
+            self.a.dealloc_array(ptr, self.cap);
+        };
+    }
+}
 
 pub trait CapacityManipulator<T> {
     fn resize(&mut self, new_len: usize, value: T);
     fn reserve(&mut self, additional: usize);
     fn shrink_to_fit(&mut self);
+}
+
+impl<T> CapacityManipulator<T> for RingBuffer<T> {
+    fn resize(&mut self, new_len: usize, value: T) {
+        
+    }
+
+    fn reserve(&mut self, additional: usize) {
+        
+    }
+
+    fn shrink_to_fit(&mut self) {
+
+    }
 }
 
 pub trait CapacityInfo {
