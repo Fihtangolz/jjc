419273c7577effb7cb3f6a2bb33ed2a809f0ead4
Dmitry <mamhigtt@gmail.com>
2019-12-10 18:57:12 +0700

    base implementation
    freeze because, cannot restrict where Self == Self::Item (err: equality constraints are not yet supported in where clauses (see #20041)), it need for implement base iterator for homogeneous trees

---
+++ algorithm/traverser.rs
@@ -0,0 +1,23 @@
+/*
+/// Dijkstra's Shortest Path First algorithm
+trait SPF {
+    type LenTable;
+    fn spf(&mut self) -> Self::LenTable;
+}
+
+impl<'a, T: GraphTraverser<'a>> SPF for DfsIterStateHolder<'a, T::Item>  
+where 
+    <T as GraphTraverser<'a>>::Item: Eq + Hash,
+{
+    type LenTable = HashMap<T::Item, usize>;
+
+    fn spf(&mut self) -> Self::LenTable {
+        let table = Self::LenTable::new();
+        // let item = self.next();
+        // let lenght = length_accessor(item);
+        // table[item] = lenght;
+
+        return table;
+    }
+}
+*/
